5d18fbf3c82cae3f3b330fee2c1ed8b6df0bf51f
Mahmood28 <mahmoodalwatani@gmail.com>
2021-01-10 12:56:15 +0300

    time for my break :)

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = 'Julio';
+let age = 60;
+let hobby = 'fishing';
+
+console.log(`Mr. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`);
